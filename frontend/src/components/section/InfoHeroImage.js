b2ac48fd157b4e294a100e0cef4640dcdf1b9bd5
Alif Wibowo <alif.likes.coding@gmail.com>
2025-02-02 19:20:15 +0700

    Add info images

---
+++ frontend/src/components/section/InfoHeroImage.js
@@ -0,0 +1,55 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { Carousel } from "antd";
+import CustomContainer from "../custom/CustomContainer";
+import { getInfoImages } from "@/app/api/public";
+import Image from "next/image";
+
+const InfoHeroImages = () => {
+  const [infoImages, setInfoImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const images = await getInfoImages();
+        setInfoImages(images);
+      } catch (error) {
+        console.error("Failed to fetch images", error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
+  return (
+    <div className="py-[12.5vh]">
+      <CustomContainer className="h-full">
+        <div className="mb-3">
+          {" "}
+          <h2 className="title2">Info</h2>
+          <h1 className="title1">Konten Informasi</h1>
+        </div>
+        <Carousel arrows infinite={true} autoplay>
+          {infoImages.map((image) => (
+            <div
+              className="bg-slate-400 flex justify-center items-center"
+              key={image.id}
+            >
+              <Image
+                src={image.image}
+                width={500}
+                height={500}
+                alt="info image"
+                views="100vw"
+                className="h-[500px] w-full"
+              />
+            </div>
+          ))}
+        </Carousel>
+      </CustomContainer>
+    </div>
+  );
+};
+
+export default InfoHeroImages;
